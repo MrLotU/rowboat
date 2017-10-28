935c56c0cd3947afc20663f2910a87a02d9c38fe
Jari (LotU) <j.koopman@jarict.nl>
2017-10-28 10:45:29 +0200

    Fix thing

--- rowboat/plugins/core.py
+++ rowboat/plugins/core.py
@@ -554,7 +554,7 @@ class CorePlugin(Plugin):
         code = cmd.func.__code__
         lines, firstlineno = inspect.getsourcelines(code)
 
-        event.msg.reply('<https://github.com/b1naryth1ef/rowboat/blob/master/{}#L{}-{}>'.format(
+        event.msg.reply('<https://github.com/MrLotU/rowboat/blob/master/{}#L{}-{}>'.format(
             code.co_filename,
             firstlineno,
             firstlineno + len(lines)
